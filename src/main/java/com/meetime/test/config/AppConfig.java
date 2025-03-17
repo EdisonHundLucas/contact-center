38bc78ebc054c19b4f56a1443afcb2a9668f844f
edisonhund <edisonhund@hotmail.com>
2025-03-17 07:29:28 -0300

    [AppConfig] Adicionando novos compos de configuração

--- src/main/java/com/meetime/test/config/AppConfig.java
+++ src/main/java/com/meetime/test/config/AppConfig.java
@@ -23,6 +23,21 @@ public class AppConfig {
     @Value("${hubspot.client-secret}")
     private String clientSecret;
 
+    @Value("${hubspot.scope}")
+    private String scope;
+
+    @Value("${hubspot.authorization-grant-type}")
+    private String authorizationGrantType;
+
+    @Value("${hubspot.redirect-uri}")
+    private String redirectUri;
+
+    @Value("${hubspot.contacts-uri}")
+    private String contactsUri;
+    
+    @Value("${hubspot.token-uri}")
+    private String tokenUri;    
+    
     // Getters e Setters
     public String getAppName() {
         return appName;
@@ -42,6 +57,46 @@ public class AppConfig {
 
     public void setClientSecret(String clientSecret) {
         this.clientSecret = clientSecret;
+    }
+
+    public String getScope() {
+        return scope;
+    }
+
+    public void setScope(String scope) {
+        this.scope = scope;
+    }
+
+    public String getAuthorizationGrantType() {
+        return authorizationGrantType;
+    }
+
+    public void setAuthorizationGrantType(String authorizationGrantType) {
+        this.authorizationGrantType = authorizationGrantType;
+    }
+
+    public String getRedirectUri() {
+        return redirectUri;
+    }
+
+    public void setRedirectUri(String redirectUri) {
+        this.redirectUri = redirectUri;
+    }
+
+    public String getContactsUri() {
+        return contactsUri;
+    }
+
+    public void setContactsUri(String contactsUri) {
+        this.contactsUri = contactsUri;
+    }
+
+    public String getTokenUri() {
+        return tokenUri;
+    }
+
+    public void setTokenUri(String tokenUri) {
+        this.tokenUri = tokenUri;
     }
 
 }
